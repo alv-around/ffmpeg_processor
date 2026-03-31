db575fbed1ef2cf6c653ab82d23b840549576eee
alv-around <alfmail@pm.me>
2026-03-31 17:01:27 +0200

    tracing

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,27 @@ use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
 use tokio::sync::mpsc::{Sender, channel};
 use tokio::{pin, select};
+use tower_http::trace::TraceLayer;
+use tracing::instrument;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                // axum logs rejections from built-in extractors with the `axum::rejection`
+                // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
+                format!(
+                    "{}=debug,tower_http=debug,axum::rejection=trace",
+                    env!("CARGO_CRATE_NAME")
+                )
+                .into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     // TODO: research the influence of the buffer size
     let (tx, mut rc) = channel::<String>(10);
     let tx = Arc::new(tx);
@@ -23,18 +41,19 @@ async fn main() {
     let app = Router::new()
         .route("/about", get(|| async { "this is an experiment" }))
         .route("/{user_id}", post(register_video))
+        .layer(TraceLayer::new_for_http())
         .with_state(tx);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    tracing::info!("listening on {}", listener.local_addr().unwrap());
     let server = axum::serve(listener, app).into_future();
-    println!("listening on 0.0.0.0:3000");
 
     pin!(server);
     loop {
         select! {
             Some(job) = rc.recv() => {
-                println!("adding job to the queue: {:?}", job);
+                tracing::trace!("adding job to the queue: {:?}", job);
                 queue.push_front(job);
             },
             _ = &mut server => {},
@@ -42,6 +61,7 @@ async fn main() {
     }
 }
 
+#[instrument]
 async fn register_video(
     State(tx): State<Arc<Sender<String>>>,
     Path(id): Path<String>,
